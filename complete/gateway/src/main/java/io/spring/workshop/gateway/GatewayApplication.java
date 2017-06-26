d1242d47ac491cab08c2c2c25912b02e24e7306b
aripka <aripka@andrews-mbp-2.roc.mn.charter.com>
2017-06-26 16:31:01 -0500

    Gateway working

--- complete/gateway/src/main/java/io/spring/workshop/gateway/GatewayApplication.java
+++ complete/gateway/src/main/java/io/spring/workshop/gateway/GatewayApplication.java
@@ -2,11 +2,21 @@ package io.spring.workshop.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
 	}
 }

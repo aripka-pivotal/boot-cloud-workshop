5d1520b65f01d7642db6d49286cdcf29510f0c40
aripka <aripka@andrews-mbp-2.roc.mn.charter.com>
2017-06-26 15:12:38 -0500

    add some code that differentiates by server ports for demo

--- complete/patient-svc/src/main/java/io/spring/workshop/patientsvc/PatientRestController.java
+++ complete/patient-svc/src/main/java/io/spring/workshop/patientsvc/PatientRestController.java
@@ -1,6 +1,9 @@
 package io.spring.workshop.patientsvc;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +12,26 @@ public class PatientRestController {
 	@Autowired
 	private PatientRepo patientRepo;
 	
+	@Value("${server.port}")
+	private int serverPort;
+	
 	@RequestMapping("/patients")
 	public Iterable<Patient> allPatients(){
+		
+		//this is janky code to demonstrate differnt behavior for different servers
+		if(serverPort%2==0){
+			ArrayList<Patient> patients = new ArrayList<>();
+			
+			for(Patient patient: patientRepo.findAll()){
+				Patient p = new Patient();
+				patients.add(p);
+				p.setId(patient.getId());
+				p.setName(patient.getName().toUpperCase());
+			}
+			
+			return patients;
+		}
+		
 		return patientRepo.findAll();
 	}
 }

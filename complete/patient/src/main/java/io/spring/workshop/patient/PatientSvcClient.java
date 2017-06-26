b1af9171d1cbea2b13126c9c59c4a916179b6f8d
aripka <aripka@andrews-mbp-2.roc.mn.charter.com>
2017-06-26 15:13:37 -0500

    make a ribbon client

--- complete/patient/src/main/java/io/spring/workshop/patient/PatientSvcClient.java
+++ complete/patient/src/main/java/io/spring/workshop/patient/PatientSvcClient.java
@@ -1,10 +1,7 @@
 package io.spring.workshop.patient;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,24 +9,18 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @Service
+@RibbonClient(name="patient-svc", configuration=PatientSvcConfig.class)
 public class PatientSvcClient {
 
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@Autowired
-	private DiscoveryClient discoveryClient;
-
 	@HystrixCommand(commandKey = "patient-patientSvc", fallbackMethod = "patientSvcFallback", commandProperties = {
 			@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "4") })
 	public PatientDetails[] allPatients() {
 
-		List<ServiceInstance> list = discoveryClient.getInstances("patient-svc");
-		if (list == null || list.size() == 0) {
-			throw new RuntimeException("No service instances found!");
-		}
 
-		return restTemplate.getForObject(list.get(0).getUri().toString() + "/patients", PatientDetails[].class);
+		return restTemplate.getForObject("http://patient-svc/patients", PatientDetails[].class);
 	}
 
 	public PatientDetails[] patientSvcFallback() {

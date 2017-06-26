f1c5b4b40099e83f71f80559747b6623a1c6db8d
aripka <aripka@andrews-mbp-2.roc.mn.charter.com>
2017-06-26 12:29:24 -0500

    add calling patient-svc discovered via Eureka

--- complete/patient/src/main/java/io/spring/workshop/patient/PatientApplication.java
+++ complete/patient/src/main/java/io/spring/workshop/patient/PatientApplication.java
@@ -2,8 +2,10 @@ package io.spring.workshop.patient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PatientApplication {
 
 	public static void main(String[] args) {
--- complete/patient/src/main/java/io/spring/workshop/patient/PatientSvcClient.java
+++ complete/patient/src/main/java/io/spring/workshop/patient/PatientSvcClient.java
@@ -1,18 +1,31 @@
 package io.spring.workshop.patient;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+
+
 @Service
 public class PatientSvcClient {
-	
+
 	@Autowired
-	private RestTemplate restTemplate; 
-		
+	private RestTemplate restTemplate;
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
 	
-		public PatientDetails[] allPatients(){
-			
-			return restTemplate.getForObject("http://localhost:8085/patients", PatientDetails[].class);
+	public PatientDetails[] allPatients() {
+
+		List<ServiceInstance> list = discoveryClient.getInstances("patient-svc");
+	       if (list == null || list.size() == 0 ) {
+	            throw new RuntimeException("No service instances found!");
+	       }
+	       
+		return restTemplate.getForObject(list.get(0).getUri().toString()+"/patients", PatientDetails[].class);
 	}
 }

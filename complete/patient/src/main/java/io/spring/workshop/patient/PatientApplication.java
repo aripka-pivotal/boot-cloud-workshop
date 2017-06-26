194656e968bc30842a121eb6ebb4a8fefa74daab
aripka <aripka@andrews-mbp-2.roc.mn.charter.com>
2017-06-26 12:53:55 -0500

    enable Hystrix Dashboard

--- complete/patient/src/main/java/io/spring/workshop/patient/PatientApplication.java
+++ complete/patient/src/main/java/io/spring/workshop/patient/PatientApplication.java
@@ -2,10 +2,12 @@ package io.spring.workshop.patient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class PatientApplication {
 
 	public static void main(String[] args) {
--- complete/patient/src/main/java/io/spring/workshop/patient/PatientSvcClient.java
+++ complete/patient/src/main/java/io/spring/workshop/patient/PatientSvcClient.java
@@ -8,7 +8,8 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @Service
 public class PatientSvcClient {
@@ -18,14 +19,20 @@ public class PatientSvcClient {
 
 	@Autowired
 	private DiscoveryClient discoveryClient;
-	
+
+	@HystrixCommand(commandKey = "patient-patientSvc", fallbackMethod = "patientSvcFallback", commandProperties = {
+			@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "4") })
 	public PatientDetails[] allPatients() {
 
 		List<ServiceInstance> list = discoveryClient.getInstances("patient-svc");
-	       if (list == null || list.size() == 0 ) {
-	            throw new RuntimeException("No service instances found!");
-	       }
-	       
-		return restTemplate.getForObject(list.get(0).getUri().toString()+"/patients", PatientDetails[].class);
+		if (list == null || list.size() == 0) {
+			throw new RuntimeException("No service instances found!");
+		}
+
+		return restTemplate.getForObject(list.get(0).getUri().toString() + "/patients", PatientDetails[].class);
+	}
+
+	public PatientDetails[] patientSvcFallback() {
+		return new PatientDetails[0];
 	}
 }

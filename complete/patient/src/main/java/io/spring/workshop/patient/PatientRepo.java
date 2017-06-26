ccd3b7a4f43972b07a2f54cb6ff651564e1c0d97
aripka <aripka@andrews-mbp-2.roc.mn.charter.com>
2017-06-26 08:38:56 -0500

    remove the JPA items

--- complete/patient/src/main/java/io/spring/workshop/patient/PatientRepo.java
+++
@@ -1,7 +0,0 @@
-package io.spring.workshop.patient;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PatientRepo extends CrudRepository<Patient, Long> {
-
-}
--- complete/patient/src/main/resources/import.sql
+++
@@ -1,2 +0,0 @@
-insert into patient (id, name) values(1, 'Andrew');
-insert into patient (id, name) values(2, 'Jesse');

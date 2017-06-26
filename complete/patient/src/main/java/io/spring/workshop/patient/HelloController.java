f02635be7b8bbbd4960087f2f6d8bccb3dfd2cc9
aripka <aripka@andrews-mbp-2.roc.mn.charter.com>
2017-06-26 11:45:04 -0500

    add a message from config server

--- complete/patient/src/main/java/io/spring/workshop/patient/HelloController.java
+++ complete/patient/src/main/java/io/spring/workshop/patient/HelloController.java
@@ -1,13 +1,24 @@
 package io.spring.workshop.patient;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class HelloController {
+	
+	@Value("${config.message:config.message not found}")
+	private String configMessage;
 	
 	@RequestMapping("/")
 	public String hello(){
 		return "Hello Spring Workshop!";
+	}
+	
+	@RequestMapping("/configMessage")
+	public String configMessage(){
+		return configMessage;
 	}
 }

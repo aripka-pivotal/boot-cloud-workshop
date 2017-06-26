2c9220941c26edc04288d6a14353766dea5ecc3f
aripka <aripka@andrews-mbp-2.roc.mn.charter.com>
2017-06-26 12:09:42 -0500

    enable patient-svc discovery

--- complete/patient-svc/src/main/java/io/spring/workshop/patientsvc/PatientSvcApplication.java
+++ complete/patient-svc/src/main/java/io/spring/workshop/patientsvc/PatientSvcApplication.java
@@ -2,8 +2,10 @@ package io.spring.workshop.patientsvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PatientSvcApplication {
 
 	public static void main(String[] args) {

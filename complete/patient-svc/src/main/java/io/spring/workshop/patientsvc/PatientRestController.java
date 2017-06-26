8998429eed7fd1f6e596ac8b156cd3daa90883f8
aripka <aripka@andrews-mbp-2.roc.mn.charter.com>
2017-06-26 08:30:46 -0500

    implement patient service

---
+++ complete/patient-svc/src/main/java/io/spring/workshop/patientsvc/PatientRestController.java
@@ -0,0 +1,16 @@
+package io.spring.workshop.patientsvc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PatientRestController {
+	@Autowired
+	private PatientRepo patientRepo;
+	
+	@RequestMapping("/patients")
+	public Iterable<Patient> allPatients(){
+		return patientRepo.findAll();
+	}
+}

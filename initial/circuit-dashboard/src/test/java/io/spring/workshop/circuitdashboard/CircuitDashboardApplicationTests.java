4b6461f9ec0a91f63bcb752a177e48ad2d7f97d4
Andrew Ripka <aripka@Andrews-MacBook-Pro-2.local>
2017-06-26 09:31:15 -0500

    circuit-dashboard start

---
+++ initial/circuit-dashboard/src/test/java/io/spring/workshop/circuitdashboard/CircuitDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package io.spring.workshop.circuitdashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CircuitDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
